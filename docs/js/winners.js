5db3b9499922a09241108db3d948f35114842b92
ZpeedTube <31473172+ZpeedTube@users.noreply.github.com>
2020-01-21 13:47:32 +0200

    First 15 winners. And small changes to css.

--- docs/js/winners.js
+++ docs/js/winners.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
         dataType: "text",
         success: function(_data) {
             let data = ProcessData(_data);
-            // alert(data[0]);
             LoadDataToTable(data);
         }
     });
@@ -18,7 +17,6 @@ function ProcessData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
     var lines = [];
-
     for (var i=1; i<allTextLines.length; i++) {
         var data = allTextLines[i].split(',');
         if (data.length == headers.length) {
@@ -27,11 +25,9 @@ function ProcessData(allText) {
                 tarr.push(data[j]);
             }
             lines.push(tarr);
-            // lines.push(allTextLines[i]);
 
         }
     }
-    // alert(lines);
     return lines;
 }
 
@@ -41,8 +37,9 @@ const colID = ["jam","name","game","theme"];
 function LoadDataToTable(data) {
     let table = $("tbody")[0];
     for(var i=0; i<data.length; i++){
-        // let tr = '<tr>';
-        let tr = document.createElement("tr");;
+        // Create new row
+        let tr = document.createElement("tr");
+        // Create a new td for each cell
         for(var col=0; col<data[i].length; col++){
             let splitD = data[i][col].split(">>>");
             const displayText = splitD[0];
@@ -51,6 +48,7 @@ function LoadDataToTable(data) {
             td.id = colID[col];
             tr.append(td);
             console.log(displayText);
+            // Checks if it has link and adds cell with link
             if (splitD.length>1) {
                 link = splitD[1];
                 let a = document.createElement("a");
@@ -59,8 +57,8 @@ function LoadDataToTable(data) {
                 td.append(a);
                 continue; // Skips to next loop (of this for loop)
             }
-            td.prepend(displayText);
-            
+            // Add normal cell
+            td.prepend(displayText);            
         }
         table.append(tr);
     }
