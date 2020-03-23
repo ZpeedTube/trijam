128dca30de7f35566247a43586a578fab560e820
ZpeedTube <31473172+ZpeedTube@users.noreply.github.com>
2020-03-23 20:49:49 +0200

    Add files via upload

---
+++ docs/js/jamlinks.js
@@ -0,0 +1,15 @@
+function Link(txt){
+    window.location = txt;
+}
+
+function TrijamNumber() {
+    const week = 7;
+    const dayoffset = 2; // Offsets days froward
+    const weekoffset = 2 * week; // Offsets 2 weeks from first jam week
+    // Tuesday of the first week of Trijam #1
+    const dateStart = Date.UTC(2019, 0, 1 + weekoffset + dayoffset);
+    const dateNow = Date.now();
+    const dif = dateNow - dateStart;
+    const calc = ((((dif / 1000) / 60) / 60) / 24) / 7;
+    return Math.ceil(calc);
+}
