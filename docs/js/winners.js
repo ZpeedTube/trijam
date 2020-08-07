c65f6aa89f23ad51b68f53c61a547116a818106b
ZpeedTube <31473172+ZpeedTube@users.noreply.github.com>
2020-08-07 16:48:20 +0300

    multi user links, added extra spaces i loop syntax

--- docs/js/winners.js
+++ docs/js/winners.js
@@ -44,12 +44,12 @@ function ProcessData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
     var lines = [];
-    for (var i=1; i<allTextLines.length; i++) {
+    for (var i = 1; i  <allTextLines.length; i++) {
         var data = allTextLines[i].split(',');
         if (data.length <= 1) continue;
         if (data.length == headers.length) {
             var tarr = [];
-            for (var j=0; j<headers.length; j++) {
+            for (var j = 0; j < headers.length; j++) {
                 tarr.push(data[j]);
             }
             lines.push(tarr);
@@ -64,28 +64,32 @@ const colID = ["jam","name","game","theme"];
 
 function LoadDataToTable(data) {
     let table = $("tbody")[0];
-    for(var i=0; i<data.length; i++){
+    for(var i = 0; i < data.length; i++){
         // Create new row
         let tr = document.createElement("tr");
         // Create a new td for each cell
-        for(var col=0; col<data[i].length; col++){
-            let splitD = data[i][col].split(">>>");
-            const displayText = splitD[0];
-            let link = "";
-            let td = document.createElement("td");
-            td.id = colID[col];
-            tr.append(td);
-            // Checks if it has link and adds cell with link
-            if (splitD.length>1) {
-                link = splitD[1];
-                let a = document.createElement("a");
-                a.href = link;
-                a.innerHTML = displayText;
-                td.append(a);
-                continue; // Skips to next loop (of this for loop)
+        for(var col = 0; col < data[i].length; col++){
+            let splitU = data[i][col].split('&&&');
+            for (const user of splitU) {
+                let userData = user.split(">>>");
+                const displayText = userData[0];
+                let link = "";
+                let td = document.createElement("td");
+                td.id = colID[col];
+                tr.append(td);
+                // Checks if it has link and adds cell with link
+                if (userData.length > 1) {
+                    link = userData[1];
+                    let a = document.createElement("a");
+                    a.href = link;
+                    a.innerHTML = displayText;
+                    td.append(a);
+                    
+                    continue; // Skips to next loop (of this for loop)
+                }                
+                // Add normal cell
+                td.prepend(displayText);            
             }
-            // Add normal cell
-            td.prepend(displayText);            
         }
         table.append(tr);
     }
